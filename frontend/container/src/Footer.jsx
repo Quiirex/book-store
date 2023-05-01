2db7b39ee44983ab5893d7d50726131e2e0f3dc4
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-01 13:12:00 +0200

    Rename

--- frontend/container/src/Footer.jsx
+++ frontend/container/src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="p-5 bg-primary text-text-base text-3xl font-bold">
-      Zavarovalnica FERI
+      Book Store
     </div>
   );
 }
